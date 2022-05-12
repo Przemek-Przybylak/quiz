dc6d0e44b76d9f66124421d6b2831d253f036424
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-12 21:51:12 +0200

    added Button component

---
+++ src/features/Button/index.js
@@ -0,0 +1,11 @@
+import {Toggler, TogglerWrapper} from "./styled";
+
+export const Button = () => {
+    return (
+        <TogglerWrapper>
+            <Toggler>
+                play
+            </Toggler>
+        </TogglerWrapper>
+    )
+}
