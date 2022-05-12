24287cbe776e5c11075ba7763df1b57905d81c8c
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-12 21:48:32 +0200

    changed height main Wrapper

--- src/features/Container/styled.js
+++ src/features/Container/styled.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Wrapper = styled.main`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-`;
+  height: 90vh;
+  overflow-y: revert`;
