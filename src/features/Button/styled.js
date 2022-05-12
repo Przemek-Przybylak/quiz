8bdf979bb058fb66f5f0b92b182f7720737bfeb2
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-12 21:49:39 +0200

    added styles for Button

---
+++ src/features/Button/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const TogglerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Toggler = styled.button`
+  color: #60aeee;
+  border: #60aeee 1px solid;
+  border-radius: 5px;
+  padding: 10px 40px;
+  background-color: white;
+  font-size: 17px;
+  width: auto;
+  font-weight: 600;
+`;
