fa17663a3b9d3b4b089e909f232a8a0d69e60f44
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-12 21:49:04 +0200

    added styles for Input & Form

---
+++ src/features/Input/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const InputBox = styled.input`
+  margin: 15px 0px;
+  padding:20px 10px;
+  width: 500px;
+  font-size: 17px;
+`;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
