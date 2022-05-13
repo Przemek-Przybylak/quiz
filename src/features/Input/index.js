e927e8128ea76cb9e6bf6e1acf9995c58760c029
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-13 17:38:42 +0200

    onSubmit event added in Input

--- src/features/Input/index.js
+++ src/features/Input/index.js
@@ -1,16 +1,25 @@
 import {useState} from "react";
 import {Form, InputBox} from "./styled";
 import {Button} from "../Button";
+import {useDispatch} from "react-redux";
+import {addPerson} from "../../common/person/personSlice";
 
 export const Input = () => {
     const [name, setName] = useState("");
+    const dispatch =useDispatch();
 
     const onChange = ({target}) => {
-        setName(target.value)
+        setName(target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        dispatch(addPerson(name))
+
     }
 
     return (
-        <Form>
+        <Form onSubmit={onSubmit}>
             <InputBox
                 value={name}
                 onChange={({target}) => onChange({target})}
