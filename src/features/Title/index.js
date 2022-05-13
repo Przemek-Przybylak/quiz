ea75c2d1f9065c85b028a3d4d3a4052c27868312
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-13 17:37:15 +0200

    added person in Title

--- src/features/Title/index.js
+++ src/features/Title/index.js
@@ -1,6 +1,10 @@
 import {Wrapper} from "./styled";
+import {useSelector} from "react-redux";
+import {selectPerson} from "../../common/person/personSlice";
 
 export const Title = ({contentTitle}) => {
+const person = useSelector(selectPerson);
+
     return (
         <Wrapper>
             {contentTitle}
