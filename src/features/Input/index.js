b84061f10e590ce08e98b8dc12e57ac884c719a7
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-15 22:30:10 +0200

    dispatch addPerson, and setApplicationStatus in onSubmit event

--- src/features/Input/index.js
+++ src/features/Input/index.js
@@ -4,6 +4,7 @@ import {Form, InputBox} from "./styled";
 import {Button} from "../../common/Button";
 import {addPerson, setApplicationStatus} from "../game/gameSlice";
 import {UseSettingCurrentState} from "../../common/UseSettingCurrentState";
+import {Title} from "../Title";
 
 export const Input = ({disabled}) => {
     const [name, setName] = useState("");
@@ -12,17 +13,18 @@ export const Input = ({disabled}) => {
 
     const onChange = ({target}) => {
         setName(target.value);
-    }
+    };
 
     const onSubmit = (event) => {
         event.preventDefault();
-        dispatch(addPerson(name))
-        dispatch(setApplicationStatus(newStatus))
-        console.log(name)
-    }
+        dispatch(addPerson(name));
+        dispatch(setApplicationStatus(newStatus));
+        console.log(name);
+    };
 
     return (<>
         <Form onSubmit={onSubmit} hidden={disabled}>
+            <Title contentTitle={"Wordcloud game"}/>
             <InputBox
                 value={name}
                 onChange={({target}) => onChange({target})}
@@ -31,4 +33,4 @@ export const Input = ({disabled}) => {
             <Button/>
         </Form>
     </>)
-}
+};
