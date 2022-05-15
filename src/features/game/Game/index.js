be2b4215a4beaf36a811a8053972b5a80b933181
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-15 16:31:18 +0200

    added content to all status in Game

--- src/features/game/Game/index.js
+++ src/features/game/Game/index.js
@@ -1,27 +1,35 @@
 import {useSelector} from "react-redux";
 import {selectStatus} from "../gameSlice";
 import {Input} from "../../Input";
+import {Button} from "../../../common/Button";
+import {GameBox} from "../GameBox";
 
 export const Game = () => {
     const status = useSelector(selectStatus);
 
     return (
         <>
-            <>
-                {status}
-                {
-                    status == "answer" ? (
-                            <Input disabled={true} >
-                            </Input>
+            {
+                status == "initial" ? (
+                        <Input/>
+                    ) :
+                    status == "answer" ? (<>
+                                <Input disabled={true}>
+                                </Input>
+                                <GameBox/>
+                                <Button click={true}/>
+                            </>
                         ) :
-                        status == "initial" ? (
-                                <Input />
-                        ) :
-                            (
-                                <div> error</div>
-                            )
-                }
-            </>
+                        status == "checkAnswers" ? (<>
+                                <Input disabled={true}>
+                                </Input>
+                                <GameBox/>
+                                <Button click={true}/>
+                            </>
+                        ) : (
+                            <div> error</div>
+                        )
+            }
         </>
     )
 }
