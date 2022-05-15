11e3708b58c5ab17941a130d7fbb9895e0d05e8a
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-15 22:30:58 +0200

    added render words in GameBox component

--- src/features/game/GameBox/index.js
+++ src/features/game/GameBox/index.js
@@ -1,8 +1,33 @@
+import {Title} from "../../Title";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchWords, selectRandomWords} from "../gameSlice";
+import {useEffect} from "react";
+import {Wrapper} from "./styled";
 
 
 export const GameBox = () => {
-    return (<>
-        gamebox
+    const words = useSelector(selectRandomWords);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchWords());
+    }, []);
+    return (
+        <>
+            <Title contentTitle={"Select something"}/>
+            {words !== undefined &&
+                <Wrapper>
+                    <div>
+                        {words.question}
+                    </div>
+                    <div>
+                        {words.all_words}
+                    </div>
+                    <div>
+                        {words.qood_words}
+                    </div>
+                </Wrapper>}
+            gamebox
         </>
     )
-}
+};
