8ba257a406d6ab948d74a155b771b038fbe7773c
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-16 15:40:50 +0200

    added content in GameBox component

--- src/features/game/GameBox/index.js
+++ src/features/game/GameBox/index.js
@@ -1,33 +1,39 @@
 import {Title} from "../../Title";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchWords, selectRandomWords} from "../gameSlice";
+import {
+    fetchWords,
+    selectRandomWords
+} from "../gameSlice";
 import {useEffect} from "react";
 import {Wrapper} from "./styled";
+import {Words} from "./Words";
 
 
 export const GameBox = () => {
-    const words = useSelector(selectRandomWords);
     const dispatch = useDispatch();
+    const words = useSelector(selectRandomWords);
 
     useEffect(() => {
         dispatch(fetchWords());
     }, []);
     return (
         <>
-            <Title contentTitle={"Select something"}/>
             {words !== undefined &&
-                <Wrapper>
-                    <div>
-                        {words.question}
-                    </div>
-                    <div>
-                        {words.all_words}
-                    </div>
-                    <div>
-                        {words.qood_words}
-                    </div>
-                </Wrapper>}
-            gamebox
+                <>
+                    <Title contentTitle={words.question}/>
+                    <Wrapper>
+                        {
+                            words.all_words &&
+                            (words.all_words).map((currentWord) => (
+                                <Words currentWord={currentWord}
+                                       words={words}
+                                >
+                                </Words>
+                            ))
+                        }
+                    </Wrapper>
+                </>
+            }
         </>
     )
 };
