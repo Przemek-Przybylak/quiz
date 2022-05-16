406ff293d266e181d7f4ef874b46a5f549d2884a
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-16 15:40:04 +0200

    added styles for words

--- src/features/game/GameBox/styled.js
+++ src/features/game/GameBox/styled.js
@@ -1,7 +1,42 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export const Wrapper = styled.section`
   width: 600px;
   height: 300px;
   border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 5px;
+  padding: 20px;
+  margin: 0;
+`;
+
+export const WordsWrapper = styled.div`
+  height: 20px;
+  margin: 10px 50px;
+  padding: 0px;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+`;
+
+export const Word = styled.button`
+  height: 10px;
+  padding: 0px;
+  margin: 0px;
+  display: flex;
+  align-items: center;
+  border: none;
+  background-color: inherit;
+  justify-content: center;
+
+  ${({good}) => good && css`
+    color: green;
+  `};
+  ${({bad}) => bad && css`
+    color: red;
+  `};
+  ${({ hidden }) => hidden && css`
+      display: none;
+`};
 `;
