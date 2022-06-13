774bf645ddd22cfd09dd762c11010a4b9369b453
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-06-13 18:54:58 +0200

    refactoring in Title component

--- src/common/Title/index.js
+++ src/common/Title/index.js
@@ -4,28 +4,11 @@ export const Title = ({contentTitle, small, finish, score}) => {
 
     return (
         <>
-            {
-                finish ? (
-                        <Wrapper finish={true}>
-                            {contentTitle}
-                        </Wrapper>
-                    ) :
-                    small ? (
-                            <Wrapper small={true}>
-                                {contentTitle}
-                            </Wrapper>
-                        ) :
-                        score ? (
-
-                            <Wrapper score={true}>
-                                {contentTitle !== 1 ? `${contentTitle} points` : `${contentTitle} point`}
-                            </Wrapper>
-                        ) : (
-                            <Wrapper>
-                                {contentTitle}
-                            </Wrapper>
-                        )
-            }
+            <Wrapper finish={finish} small={small} score={score}>
+                {score ?
+                    contentTitle !== 1 ? `${contentTitle} points` : `${contentTitle} point`
+                    : contentTitle}
+            </Wrapper>
         </>
     )
 };
