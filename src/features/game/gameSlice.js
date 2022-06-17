bd665017344f01c2dbfa754e7c8774158fd7180f
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-06-17 21:09:29 +0200

    added cklicked words to state

--- src/features/game/gameSlice.js
+++ src/features/game/gameSlice.js
@@ -7,6 +7,7 @@ const initialState = {
     words: [],
     number: Math.floor(Math.random() * 3),
     points: 0,
+    clickedWords: [],
 }
 
 const gameSlice = createSlice({
@@ -32,6 +33,9 @@ const gameSlice = createSlice({
         },
         addPoint: (state) => {
             state.points = state.points + 1
+        },
+        addClickedWord: (state, {payload}) => {
+            state.clickedWords.push(payload)
         }
     }
 });
@@ -43,12 +47,14 @@ export const {
     fetchWordsSuccess,
     fetchWordsError,
     addPoint,
+    addClickedWord,
 } = gameSlice.actions;
 
 export const selectStatus = (state) => state.game.gameStatus;
 export const selectPerson = (state) => state.game.personName;
 export const selectWords = (state) => state.game.words;
 export const selectPoints = (state) => state.game.points;
+export const selectClickedWords = (state) => state.game.clickedWords;
 
 export const selectRandomWords = (state) => {
     return state.game.words[state.game.number];
